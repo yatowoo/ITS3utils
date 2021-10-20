88f24c5556cf6750f4877c6fbb4d32d00c9c63d9
yatowoo <torrence@mail.ustc.edu.cn>
2021-10-20 21:41:33 +0200

    PS202110 - fix env

--- PS202110/env_beam.sh
+++ PS202110/env_beam.sh
@@ -8,12 +8,13 @@ fi
 
 export TRIGGER_PORT=/dev/serial/by-id/usb-CERN_ITS3_Trigger_Board_210512_1140-if01-port0
 export DATA_DIR=/media/T7/PS202110
+export ALPIDE_DAQ_FW=~/alpide-daq-software/
 
 alias ll='ls -l -h --group-directories-first'
 alias senv='vim ~/.bashrc'
 alias uenv='source ~/.bashrc'
 alias alpide-ls='alpide-daq-program --list'
-alias alpide-flash-all='alpide-daq-program --all --fx3 ~/alpide-daq-software/fx3.img --fpga ~/alpide-daq-software/fpga-v1.0.0.bit'
+alias alpide-flash-all='alpide-daq-program --all --fx3 $ALPIDE_DAQ_FW/fx3.img --fpga $ALPIDE_DAQ_FW/fpga-v1.0.0.bit'
 alias gorun='cd $RUN_DIR'
 alias godata='cd $DATA_DIR'
 alias srun='./ITS3start.sh && tmux a -t ITS3'
