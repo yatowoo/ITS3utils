2f0e7baf89cafc0bc8645e7464068403e1364a77
yatowoo <torrence@mail.ustc.edu.cn>
2022-06-08 23:48:57 +0800

    QA - add noise plots

--- eudaq/analog_qa.py
+++ eudaq/analog_qa.py
@@ -17,6 +17,7 @@ parser.add_argument(
     '-s', '--signal', help='Signal method', default='cds')
 parser.add_argument('--taf', help='Noise map from TAF', type=str, default=None)
 parser.add_argument('--mask', help='Mask file types', type=str, default='RTS,hot,bad')
+parser.add_argument('--rts', help='Strict mode to identify pixels with RTS effect', default=False, action='store_true')
 parser.add_argument('--frac', help='Fraction inside 3 sigma, used for RTS', type=float, default=0.95)
 parser.add_argument('--debug','-v', help='Debug for fitting by pixels', default=False, action='store_true')
 
@@ -25,13 +26,13 @@ args = parser.parse_args()
 NX, NY = 64, 32
 SUBMATRIX_EDGE = [21, 42, 64]
 SUBMATRIX_NOISE_CUT = [200, 200, 30]
-SUBMATRIX_BINWIDTH = [10, 10, 5]
+SUBMATRIX_BINWIDTH = [10, 10, 2]
 NOISE_THRESHOLD = 500
 
 qaFile = ROOT.TFile.Open(args.input)
 hqa = qaFile.Get(f"h2qa_{args.signal}")
-c = ROOT.TCanvas('c1','Noise QA', 1440, 1200)
-c.Divide(2,2)
+c = ROOT.TCanvas('c1','Noise QA', 2400, 1200)
+c.Divide(3,3)
 c.cd(1)
 hqa.Draw('colz')
 c.cd(2)
@@ -43,8 +44,9 @@ gRMS = ROOT.TGraph()
 hSigma = ROOT.TH1F('hsigma','#sigma (gaus fitting);ADCu;# pixels',100,0,500)
 hFWHM = ROOT.TH1F('hFWHM','FWHM/2;ADCu;# pixels',100,0,500)
 gSigma = ROOT.TGraph()
-hDiff = ROOT.TH1F('hdiff','Noise difference;ADCu;# pixels',1000,-500,500)
+hDiff = ROOT.TH1F('hdiff','Noise relative difference;#Delta_{rel}#sigma = (#sigma - ref)/#sigma;# pixels',100,-5,5)
 hFrac = ROOT.TH1F('hfrac','Fraction of counts in 3#sigma region;Fraction_{3#sigma};# pixels', 100, 0.01, 1.01)
+hChiFit = ROOT.TH1F('hchifit','Gaussian fitting quality;#chi^{2} / Ndf;# pixels', 1000, 0., 10.)
 hPedestalMap = ROOT.TH2D('hPedestalpl1','Map of pixel pedestal;Pxiel X;Pixel Y;#pixels',
   64,-0.5,63.5,32,-0.5,31.5)
 hNoiseMap = ROOT.TH2D('hnoisepl1','Map of pixel noise amplitude;Pxiel X;Pixel Y;#pixels',
@@ -90,6 +92,9 @@ def noise_fit(qadb):
   qadb['FWHM'] = qadb['noise'].GetBinCenter(halfRight) - qadb['noise'].GetBinCenter(halfLeft)
   fitRange = min(0.8 * qadb['FWHM'], 2*qadb['rms'])
   resultPtr = qadb['noise'].Fit(fitfcn,"SQ","",qadb['noise'].GetMean()-fitRange, qadb['noise'].GetMean()+fitRange)
+  drawRange = min(2000, fitRange * 30)
+  qadb['noise'].GetXaxis().SetRangeUser(-drawRange, drawRange)
+  qadb['noise'].SetYTitle(f'# events / ({qadb["noise"].GetBinWidth(1):.0f} ADCu)')
   qadb['chi2'] = resultPtr.Chi2()
   qadb['ndf'] = resultPtr.Ndf()
   qadb['mean'] = fitfcn.GetParameter(1)
@@ -104,9 +109,9 @@ def noise_fit(qadb):
     qadb['noise'].Rebin(2)
     return noise_fit(qadb)
   # Draw info.
-  pave = newObj(ROOT.TPaveText(0.15, 0.6, 0.45, 0.88,'NDC'))
+  pave = newObj(ROOT.TPaveText(0.15, 0.4, 0.45, 0.85,'NDC'))
   pave.SetFillColor(ROOT.kWhite)
-  add_text(pave, qadb['type'], font=62)
+  add_text(pave, qadb['type'], font=62, size=0.10)
   add_text(pave, f'#sigma_{{fit}} = {qadb["sigma"]:.1f}')
   add_text(pave, f'#chi^{{2}} / NDF = {qadb["chi2"]:.1f} / {qadb["ndf"]}')
   add_text(pave, f'fraction_{{3#sigma}} = {qadb["frac"]:.3f}')
@@ -118,6 +123,10 @@ def noise_type(qadb):
   if(qadb['sigma'] > NOISE_THRESHOLD):
     return 'bad'
   cut = SUBMATRIX_NOISE_CUT[submatrix_id(qadb['pos'][0],qadb['pos'][1])]
+  if(args.rts):
+    if qadb['frac'] < args.frac: return 'RTS'
+    if qadb['ndf'] > 0 and qadb['chi2'] / qadb['ndf'] > 3.0:
+      return 'RTS'
   if qadb['rms'] < cut:
     return 'normal'
   elif qadb['sigma'] < cut:
@@ -130,7 +139,7 @@ if(args.taf is not None):
     with ROOT.TFile.Open(args.taf) as tafFile:
       mapTAF = tafFile.Get('hnoisepl1').Clone('h2noiseTAF')
       hTAF = tafFile.Get('hNoiseDistripl1').Clone('hnoiseTAF')
-      hDiffTAF = ROOT.TH1F('hdiffTAF','Noise difference',1000,-500,500)
+      hDiffTAF = ROOT.TH1F('hdiffTAF','Noise relative difference',1000,-500,500)
     args.taf = True
 else:
   args.taf = False
@@ -163,26 +172,54 @@ for ipx in range(1,2048+1):
   hSigma.Fill(qadb['sigma'])
   hFWHM.Fill(qadb['FWHM'] * 0.5)
   hFrac.Fill(qadb['frac'])
+  if(qadb['ndf'] == 0):
+    print(f'[+] ({ix}, {iy}) bad fitting with NDF=0')
+    hChiFit.Fill(-1.)
+  else:
+    hChiFit.Fill(qadb['chi2'] / qadb['ndf'])
   # Difference
   noiseComp = f'> ({ix}, {iy}) {qadb["type"]} pixel found - RMS={qadb["rms"]:.1f} | Sigma={qadb["sigma"]:.1f} | FWHM/2={qadb["FWHM"]*0.5:.1f} |'
   if(args.taf):
     qadb['taf'] = mapTAF.GetBinContent(ix+1, iy+1)
-    hDiffTAF.Fill(qadb['sigma'] - qadb['taf'])
+    hDiffTAF.Fill((qadb['sigma'] - qadb['taf']) / qadb['sigma'])
     noiseComp  = noiseComp + f' TAF={qadb["taf"]:.1f} |'
-  hDiff.Fill(qadb['sigma'] - qadb['rms'])
+  hDiff.Fill( (qadb['sigma'] - qadb['rms']) / qadb['sigma'])
   if(args.debug):
+    flagDebug = False
+    # Mask pixel
+    if qadb['type'].lower() in args.mask.lower(): flagDebug = True
+    # chi2 / ndf
+    if(qadb['ndf'] == 0 or qadb['chi2'] / qadb['ndf'] > 3.0):
+      flagDebug = True
+    # RTS more
     if(qadb['type'] == 'normal' and qadb['frac'] < args.frac):
-      qadb['type'] = 'RTS'
-  if(args.debug and qadb['type'].lower() in args.mask.lower()):
+      flagDebug = True
+    # STOP and show
+    if(not flagDebug): continue
     resultPtr.Print()
     print(noiseComp)
     ROOT.gPad.Update()
     cmd = input()
     
 # Drawing and output
-c.cd(3)
+
+# noise as profile in TH2D of all pixels
+c.cd(1)
+gRMS.SetMarkerColor(ROOT.kBlack)
+gRMS.SetLineColor(ROOT.kBlack)
+gRMS.SetDrawOption("ALP")
+gSigma.SetMarkerColor(ROOT.kRed)
+gSigma.SetLineColor(ROOT.kRed)
+gSigma.SetDrawOption("ALP")
+gRMS.Draw("same")
+gSigma.Draw("same")
+
+padIndex = 3
+c.cd(padIndex)
 hRMS.SetLineColor(ROOT.kBlack)
 hRMS.Draw()
+ROOT.gPad.SetLogy(True)
+hRMS.GetYaxis().SetRangeUser(0.1, 10 * hRMS.GetMaximum())
 hSigma.SetLineColor(ROOT.kRed)
 hSigma.Draw('same')
 hFWHM.SetLineColor(ROOT.kBlue)
@@ -194,19 +231,11 @@ if(args.taf):
 ROOT.gPad.BuildLegend(0.5,0.6,0.8,0.8)
 ROOT.gStyle.SetOptStat(0)
 
-c.cd(1)
-gRMS.SetMarkerColor(ROOT.kBlack)
-gRMS.SetLineColor(ROOT.kBlack)
-gRMS.SetDrawOption("ALP")
-gSigma.SetMarkerColor(ROOT.kRed)
-gSigma.SetLineColor(ROOT.kRed)
-gSigma.SetDrawOption("ALP")
-gRMS.Draw("same")
-gSigma.Draw("same")
-
-c.cd(4)
+padIndex += 1
+c.cd(padIndex)
 hDiff.Draw()
-hDiff.SetTitle('Noise difference (#sigma - RMS)')
+ROOT.gPad.SetLogy(True)
+hDiff.SetTitle('Noise relative difference (#sigma vs RMS)')
 if(args.taf):
   hDiffTAF.SetLineColor(ROOT.kRed)
   hDiffTAF.Draw('same')
@@ -215,6 +244,34 @@ if(args.taf):
   lgd.AddEntry(hDiff, '#sigma - RMS')
   lgd.AddEntry(hDiffTAF, '#sigma - TAF')
   lgd.Draw('same')
+
+padIndex += 1
+c.cd(padIndex)
+hFrac.Draw()
+ROOT.gPad.SetLogy(True)
+
+padIndex += 1
+c.cd(padIndex)
+hChiFit.Draw()
+pave = newObj(ROOT.TPaveText(0.6,0.5,0.85,0.7,'NDC'))
+pave.SetFillColor(ROOT.kWhite)
+add_text(pave, f'Overflow: {hChiFit.GetBinContent(hChiFit.GetNbinsX()+1):.0f}',size=0.06)
+pave.Draw('same')
+ROOT.gPad.SetLogy(True)
+
+padIndex += 1
+c.cd(padIndex)
+hPedestalMap.Draw("colz")
+
+padIndex += 1
+c.cd(padIndex)
+hNoiseMap.Draw("colz")
+
+padIndex += 1
+c.cd(padIndex)
+hNoiseMapType.Draw("colz")
+
+c.Update()
 
 # Summary
 typeSum = [qadb['type'] for qadb in pixel_qa]
@@ -234,6 +291,7 @@ with open(f'{args.output}_mask.txt','w') as maskFile:
   print(f'[-] Mask file write to {maskFile.name} ({args.mask})')
 
 cmd = input("Exit after checking plots: <type enter>")
+c.SaveAs(f'{args.output}.pdf')
 
 outputFile = ROOT.TFile(f'{args.output}.root','RECREATE')
 outputFile.cd()
@@ -243,8 +301,6 @@ hNoiseMap.Write()
 hPedestalMap.Write()
 hNoiseMapType.Write()
 hFrac.Write()
+hChiFit.Write()
 outputFile.Close()
 qaFile.Close()
-
-c.SaveAs(f'{args.output}.pdf')
-
